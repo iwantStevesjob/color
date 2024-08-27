4c1b8c71a257507fb1955863bb251ad244c1b5cc
Steves job <66954787+iwantStevesjob@users.noreply.github.com>
2024-08-27 17:16:16 -0500

    Update peer.js

--- peer.js
+++ peer.js
@@ -1,107 +1,95 @@
-const PeerModule = (function() {
-    let peer, connections = [];
-    let isServer = false;
-    let contentCallback = null;
-    let statusCallback = null;
-    let peerCountCallback = null;
+let peer, connections = [];
+let isServer = false;
+let lastSyncedContent = '';
 
-    function init(options) {
-        statusCallback = options.onStatusChange;
-        peerCountCallback = options.onPeerCountChange;
-        contentCallback = options.onContentChange;
+function initPeerJS() {
+    const hash = window.location.hash;
 
-        const hash = window.location.hash;
+    peer = new Peer();
 
-        peer = new Peer();
+    peer.on('open', (id) => {
+        updateConnectionStatus(true);
+        console.log(`Peer ID: ${id}`);
 
-        peer.on('open', (id) => {
-            updateStatus(true);
-            console.log(`Peer ID: ${id}`);
+        if (!hash) {
+            isServer = true;
+            window.location.hash = id;
+            initNotes(); // Initialize notes only for the server
+        } else {
+            const remotePeerId = hash.slice(1);
+            connectToPeer(remotePeerId);
+        }
+    });
 
-            if (!hash) {
-                isServer = true;
-                window.location.hash = id;
-            } else {
-                const remotePeerId = hash.slice(1);
-                connectToPeer(remotePeerId);
-            }
-        });
+    peer.on('connection', handleNewConnection);
 
-        peer.on('connection', handleNewConnection);
+    peer.on('error', (err) => {
+        console.error('PeerJS Error:', err);
+        updateConnectionStatus(false);
+        alert('An error occurred with the P2P connection: ' + err.message);
+    });
+}
 
-        peer.on('error', (err) => {
-            console.error('PeerJS Error:', err);
-            updateStatus(false);
-            alert('An error occurred with the P2P connection: ' + err.message);
+function handleNewConnection(connection) {
+    connections.push(connection);
+    setupConnectionListeners(connection);
+    updatePeerCount();
+
+    if (isServer) {
+        connection.on('open', () => {
+            connection.send({ type: 'content', data: display.innerHTML });
         });
     }
+}
 
-    function handleNewConnection(connection) {
-        connections.push(connection);
-        setupConnectionListeners(connection);
+function connectToPeer(peerId) {
+    const conn = peer.connect(peerId);
+    connections.push(conn);
+    setupConnectionListeners(conn);
+}
+
+function setupConnectionListeners(conn) {
+    conn.on('open', () => {
+        console.log('Connected to peer');
+        updateConnectionStatus(true);
+    });
+
+    conn.on('data', handleIncomingData);
+
+    conn.on('close', () => {
+        console.log('Connection closed, attempting to reconnect...');
+        updateConnectionStatus(false);
+        connections = connections.filter(c => c !== conn);
         updatePeerCount();
-
-        if (isServer) {
-            connection.on('open', () => {
-                const content = contentCallback();
-                connection.send({ type: 'content', data: content });
-            });
+        if (!isServer) {
+            setTimeout(() => connectToPeer(conn.peer), 3000);
         }
+    });
+}
+
+function handleIncomingData(data) {
+    if (data.type === 'content') {
+        display.innerHTML = data.data;
+        lastSyncedContent = data.data;
+        const peerId = window.location.hash.slice(1);
+        saveContentToStorage(peerId, data.data);
     }
+}
 
-    function connectToPeer(peerId) {
-        const conn = peer.connect(peerId);
-        connections.push(conn);
-        setupConnectionListeners(conn);
-    }
-
-    function setupConnectionListeners(conn) {
-        conn.on('open', () => {
-            console.log('Connected to peer');
-            updateStatus(true);
-        });
-
-        conn.on('data', handleIncomingData);
-
-        conn.on('close', () => {
-            console.log('Connection closed, attempting to reconnect...');
-            updateStatus(false);
-            connections = connections.filter(c => c !== conn);
-            updatePeerCount();
-            if (!isServer) {
-                setTimeout(() => connectToPeer(conn.peer), 3000);
-            }
-        });
-    }
-
-    function handleIncomingData(data) {
-        if (data.type === 'content') {
-            contentCallback(data.data);
-        }
-    }
-
-    function broadcastData(content) {
+function broadcastData() {
+    const content = display.innerHTML;
+    if (content !== lastSyncedContent) {
+        lastSyncedContent = content;
+        const peerId = window.location.hash.slice(1);
+        saveContentToStorage(peerId, content);
         connections.forEach(conn => {
             if (conn.open) {
                 conn.send({ type: 'content', data: content });
             }
         });
     }
+}
 
-    function updateStatus(connected) {
-        if (statusCallback) {
-            statusCallback(connected);
-        }
-    }
-
-    function updatePeerCount() {
-        if (peerCountCallback) {
-            peerCountCallback(connections.length);
-        }
-    }
-
-    return {
-        init: init,
-        broadcastData: broadcastData
-    };
-})();
+// Exported functions to be used in main.js
+window.initPeerJS = initPeerJS;
+window.broadcastData = broadcastData;
